429c2c3ddd53ead0bd20b8ac1bf024b575447c6c
andrew moore <andrew@lessthanseventy.com>
2019-07-21 11:45:10 -0400

    initial commit

---
+++ _javascript/main.js
@@ -0,0 +1,167 @@
+"use strict";
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const Markov = require("markov-generator");
+
+const accessToken =
+  "?access_token=KuENUxCaPSBuM168bMWkyIkpXELItXXfk-smz15ChsPwNA5zAKr5Rg5VxHvfol17";
+
+document.onload = init();
+
+function init() {
+  document.getElementById("inputButton").addEventListener("click", onSubmit);
+
+  async function onSubmit() {
+    let element = document.getElementById("loadingBar");
+    element.classList.remove("is-hidden");
+    let inputValue = document.getElementById("inputForm").value.toString();
+    await artistByName(inputValue)
+      .then(res => loopForLyrics(res))
+      .then(lyrics => generateSong(lyrics));
+  }
+
+  async function _request(path) {
+    const url = `https://api.genius.com/${path}`;
+
+    // Fetch result and parse it as JSON
+    const body = await fetch(url);
+    const result = await body.json();
+
+    // Handle errors
+    if (result.error)
+      throw new Error(`${result.error}: ${result.error_description}`);
+    if (result.meta.status !== 200)
+      throw new Error(`${result.meta.status}: ${result.meta.message}`);
+
+    return result.response;
+  }
+
+  /*
+Get song by ID
+*/
+
+  async function song(id, { fetchLyrics = false, textFormat = "dom" } = {}) {
+    if (!id) throw new Error("No ID was provided to lyricist.song()");
+
+    const path = `songs/${id}${accessToken}&?text_format=${textFormat}`;
+    const { song } = await _request(path);
+
+    const lyrics = fetchLyrics ? await _scrapeLyrics(song.url) : null;
+
+    return Object.assign({ lyrics }, song);
+  }
+
+  /* Get artist by ID */
+
+  async function artist(id, { textFormat = "dom" } = {}) {
+    if (!id) throw new Error("No ID was provided to lyricist.artist()");
+
+    const path = `artists/${id}${accessToken}&?text_format=${textFormat}`;
+    const { artist } = await _request(path);
+    return artist;
+  }
+
+  async function artistByName(name) {
+    const slug = _geniusSlug(name);
+    const id = await _scrapeArtistPageForArtistID(slug);
+    console.log(id);
+    return songsByArtist(id);
+  }
+
+  /* Get artist songs */
+
+  async function songsByArtist(
+    id,
+    { page = 1, perPage = 10, sort = "popularity" } = {}
+  ) {
+    if (!id) throw new Error("No ID was provided to lyricist.songsByArtist()");
+
+    const path = `artists/${id}/songs${accessToken}&per_page=${perPage}&page=${page}&sort=${sort}`;
+    const { songs } = await _request(path);
+
+    return songs;
+  }
+
+  /* Scrape song lyrics */
+
+  async function _scrapeLyrics(url) {
+    const newUrl = `http://localhost:8080/${url}`;
+    const response = await fetch(newUrl);
+    const text = await response.text();
+    const $ = cheerio.load(text);
+    return $(".lyrics")
+      .text()
+      .trim();
+  }
+
+  /* Get slug from name/title */
+
+  function _geniusSlug(string) {
+    // Probably not 100% accurate yet
+    // Currently only used by undocumented artistByName function
+    const slug = string
+      .trim()
+      .replace(/\s+/g, "-")
+      .replace("'", "")
+      .replace("&", "and")
+      .replace(/[^a-zA-Z0-9]/g, "-")
+      .toLowerCase();
+    // Uppercase first letter
+    return slug.charAt(0).toUpperCase() + slug.slice(1);
+  }
+
+  /* Scrape artist page to retrieve artist ID */
+
+  async function _scrapeArtistPageForArtistID(slug) {
+    const url = `http://localhost:8080/genius.com/artists/${slug}`;
+    const html = await fetch(url).then(res => res.text());
+    const $ = cheerio.load(html);
+    const id = $('meta[name="newrelic-resource-path"]')
+      .attr("content")
+      .split("/")
+      .pop();
+    return id;
+  }
+
+  /* Loop through array of song ids and return cleaned array of strings to pass to Markov Generator */
+  async function loopForLyrics(arr) {
+    let temp = [];
+    let lyricsArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      await song(arr[i].id, { fetchLyrics: true }).then(function(res) {
+        /* remove [Verse] and such and any parentheses */
+        temp.push(
+          res.lyrics.replace(/ *\[[^\]]*\] */g, "").replace(/[\(\)] /g, "")
+        );
+      });
+    }
+    /* Split temp array at newllnes an push to output array */
+    for (let i = 0; i < temp.length; i++) {
+      let a = temp[i].split("\n");
+      for (let j = 0; j < a.length; j++) {
+        lyricsArray.push(a[j]);
+      }
+    }
+    return lyricsArray;
+  }
+
+  /* Generate song using passed in array of lyrics */
+  async function generateSong(arrayOfLyrics) {
+    let html = "";
+    for (let i = 0; i < 30; i++) {
+      let markov = new Markov({
+        input: arrayOfLyrics,
+        minLength: 8
+      });
+      let sentence = markov.makeChain();
+      if (i != 0 && i % 4 == 0) {
+        html += "<div><br></div>";
+      }
+      html += "<div>" + sentence + "</div>";
+    }
+    document.getElementById("loadingBar").classList.add("is-hidden");
+    document.getElementById("lyrics").innerHTML = html;
+  }
+  // test
+  //   .then(res => loopForLyrics(res))
+}
